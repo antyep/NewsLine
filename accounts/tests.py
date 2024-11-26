22a17bcea915faff21fabb7e3ad46cb55b6e4976
antyep <antyep6@gmail.com>
2024-11-26 18:00:35 +0100

    test: sign up page exists added

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class SignupPageTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
