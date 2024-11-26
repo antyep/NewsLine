c02db235737f6d70e379c67af00746dd9d081542
antyep <antyep6@gmail.com>
2024-11-26 15:19:39 +0100

    fix(urls): add include on accounts path to resolve module not found error

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,6 +22,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("accounts/", ("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
