bdd138840b806dbcf7bb32f14c06ddc906a81dc5
antyep <antyep6@gmail.com>
2024-11-26 18:03:11 +0100

    test: add t est for sign up view and sign up template

--- accounts/tests.py
+++ accounts/tests.py
@@ -9,3 +9,8 @@ class SignupPageTest(TestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/accounts/signup/")
         self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
